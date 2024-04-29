969d0f7fe7d054fafb65e3d04ea32bc3da737e69
AmirSeraj <amir.seraj6990@gmail.com>
2024-04-30 01:22:15 +0330

    frregt

--- components/Header/Tabs.tsx
+++ components/Header/Tabs.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 
 const Tabs = async ({ t }: any) => {
+  const session = true;
   return (
     <div className="gap-7 items-center text-white text-[15px] md:flex hidden">
-      <Link href="/">Solution</Link>
+      {session ? (
+        <>
+          <Link href={"/dahboard"}>Amir</Link>
+          <div>{t("logout")}</div>
+        </>
+      ) : (
+        <Link href={"/login"}>{t("login")}</Link>
+      )}
       <Link href="/products">{t("products")}</Link>
-      <Link href="/blogs">{t("common:blog")}</Link>
-      <Link href="/docs">{t("common:docs")}</Link>
-      <Link href="/about">{t("common:about_us")}</Link>
+      <Link href="/blogs">{t("blog")}</Link>
+      <Link href="/docs">{t("docs")}</Link>
+      <Link href="/about">{t("about_us")}</Link>
     </div>
   );
 };
